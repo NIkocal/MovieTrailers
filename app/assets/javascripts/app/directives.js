539465bc6135ce18f3f89ea698244012bfd049fe
Nikolai <redlacialokin@gmail.com>
2015-08-16 21:30:34 -0400

    add currentUser and logout functionality to directive

--- app/assets/javascripts/app/directives.js
+++ app/assets/javascripts/app/directives.js
@@ -2,8 +2,17 @@ angular.module('popcornApp.directives', [])
 .directive('userPanel', function() {
 	return {
 		templateUrl: '/templates/user_panel.html',
-		controller: function(){
-			
+		controller: function($scope, UserService){
+			UserService.currentUser().then(function (user) {
+				$scope.user = user;
+			});
+
+			$scope.logout = function () {
+				UserService.logout().then(function() {
+					$scope.currentUser = null;
+				});
+			};
+
 		}
-	}
+	};
 });
