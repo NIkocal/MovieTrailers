3f2f1e2bf4b9e8d7ccb1f116153a11475f882f5d
Nikolai <redlacialokin@gmail.com>
2015-08-16 21:16:07 -0400

    fixed path

--- app/assets/javascripts/app/directives.js
+++ app/assets/javascripts/app/directives.js
@@ -1,4 +1,9 @@
 angular.module('popcornApp.directives', [])
 .directive('userPanel', function() {
-
+	return {
+		templateUrl: '/templates/user_panel.html',
+		controller: function(){
+			
+		}
+	}
 });
